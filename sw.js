9d13b1f50f15053583020d471a2dd52b9f8b648b
Karima-Kaddouri <kari.kaddouri@gmail.com>
2025-10-08 15:35:27 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'headsup-fr-cache-v1';
+const urlsToCache = ['index.html', 'manifest.json', 'icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
